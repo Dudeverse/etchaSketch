0722358df9d3f1a44fec0204e368f375f8e52d40
Surya Teja <tejasurya7117@gmail.com>
2024-06-20 15:36:23 -0500

    TODO flexible pixels

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-
+// ------ IMPORTANT GLOBAL VARIABLES
 
 let grid = document.querySelector(".container");
-const resetBtn = document.querySelector(".reset");
-resetBtn.addEventListener('click', resetBoard);
+let gridSize = 8; 
 
-function generateGrid() {
-    for (let i=0; i<16; i++) {
+function startCanvas(gridSize) {
+    grid.innerHTML = "";
+    generateGrid(gridSize);
+    paintBrush();
+}
+startCanvas(gridSize);
+
+// ------ CANVAS GENERATION FUNCTIONALITY
+
+function generateGrid(gridSize) {
+    for (let i=0; i<gridSize; i++) {
         let row = document.createElement("div");
         row.style.display = "flex";
         row.style.justifyContent = "center";
         row.style.alignItems="center";
         row.className = "row";
-        for (let j=0; j<=16; j++) {
+        for (let j=0; j<gridSize; j++) {
             let pixel = document.createElement("div");
+            pixel.style.display="flex";
             pixel.className="pixel";
             row.append(pixel);
-            pixel.style.border = "1px solid black"
             pixel.style.padding = "10px"
+            pixel.style.border = "1px solid black"
             // pixel.addEventListener("mouseover", (event) => {
             //     pixel.style.backgroundColor = "red";
             // });
@@ -24,8 +33,7 @@ function generateGrid() {
         grid.append(row);
     }
 }
-
-generateGrid();
+// -----PAINTBRUSH FUNCTIONALITY
 
 function paintBrush() {
     document.querySelectorAll(".pixel").forEach((element) => {
@@ -34,15 +42,27 @@ function paintBrush() {
         });
     });
 }
-
-paintBrush()
-
+// -----COLORING-EFFECT
 function changeBackgroundColor(element) {
     element.style.backgroundColor = "red";
 }
+// ------ SET GRID SIZE FUNCTIONALITY
+const setGridBtn = document.querySelector(".setgrid");
+setGridBtn.addEventListener('click', setGrid);
+function setGrid() {
+    previousGridSize = gridSize;
+    gridSize = prompt("Enter a grid size (Range: 2-100)")
+    if (gridSize === null) {
+        gridSize=previousGridSize; //break out of the function early
+    }
+    grid.innerHTML = "";
+    startCanvas(gridSize)
+}
 
+//----- RESET BUTTON FUNCTIONALITY
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener('click', resetBoard);
 function resetBoard() {
     grid.innerHTML = "";
-    generateGrid();
-    paintBrush();
+    startCanvas(gridSize)
 }
